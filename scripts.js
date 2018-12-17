bc32d43770765d0e0af75778f1bf0abf0c0bd7cf
RGB Studios <justingolden21@gmail.com>
2018-12-16 20:45:48 -0800

    highlight hungry fish

--- scripts.js
+++ scripts.js
@@ -332,25 +332,45 @@ function updateUI() {
 	$('#num-coin-rate').html(num_coin_rate);
 	$('#num-food-rate').html(all_fish.length - num_hungry_fish);
 	$('#num-hungry-fish').html(num_hungry_fish);
+	if(num_hungry_fish>0) {
+		$('#num-hungry-fish').addClass('highlight');
+	} else {
+		$('#num-hungry-fish').removeClass('highlight');
+	}
 	
 	$('#num-small-fish').html(small_fish.length);
 	$('#num-small-fish-food-rate').html(small_fish.length - num_hungry_small_fish);
 	$('#num-small-fish-coin-rate').html( (small_fish.length - num_hungry_small_fish)*SMALL_FISH_COIN);
 	$('#num-small-fish-space-total').html(small_fish.length*SMALL_FISH_SPACE);
 	$('#num-small-fish-hungry').html(num_hungry_small_fish);
+	if(num_hungry_small_fish>0) {
+		$('#num-small-fish-hungry').addClass('highlight');
+	} else {
+		$('#num-small-fish-hungry').removeClass('highlight');
+	}
 
 	$('#num-medium-fish').html(medium_fish.length);
 	$('#num-medium-fish-food-rate').html(medium_fish.length - num_hungry_medium_fish);
 	$('#num-medium-fish-coin-rate').html( (medium_fish.length - num_hungry_medium_fish)*MEDIUM_FISH_COIN);
 	$('#num-medium-fish-space-total').html(medium_fish.length*MEDIUM_FISH_SPACE);
 	$('#num-medium-fish-hungry').html(num_hungry_medium_fish);
+	if(num_hungry_medium_fish>0) {
+		$('#num-medium-fish-hungry').addClass('highlight');
+	} else {
+		$('#num-medium-fish-hungry').removeClass('highlight');
+	}
 
 	$('#num-big-fish').html(big_fish.length);
 	$('#num-big-fish-food-rate').html(big_fish.length - num_hungry_big_fish);
 	$('#num-big-fish-coin-rate').html( (big_fish.length - num_hungry_big_fish)*BIG_FISH_COIN);
 	$('#num-big-fish-space-total').html(big_fish.length*BIG_FISH_SPACE);
 	$('#num-big-fish-hungry').html(num_hungry_big_fish);
-	
+	if(num_hungry_big_fish>0) {
+		$('#num-big-fish-hungry').addClass('highlight');
+	} else {
+		$('#num-big-fish-hungry').removeClass('highlight');
+	}
+
 	$('#num-aquarium').html(num_aquarium);
 	$('#num-aquarium-space-total').html(num_aquarium * AQUARIUM_SPACE);
 	$('#num-aquarium-space-used').html(num_aquarium_space_used);
@@ -384,9 +404,9 @@ function showSnackbar(message, type) {
 	// $('#snackbar').css('animation', 'fadein 0.5s, fadeout 0.5s 2.5s');
 	setTimeout(()=> { $('#snackbar').removeClass('show') }, 3000);
 }
-function showHighlight(elm) {
+function showHighlight(elm, sec=0.5) {
 	elm.addClass('highlight');
-	setTimeout(()=> { elm.removeClass('highlight') }, 500);
+	setTimeout(()=> { elm.removeClass('highlight') }, sec*1000);
 }
 function handleResize() {
 	if(window.innerWidth <= 1024) {
