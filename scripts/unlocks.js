94a81aa7d9cce75452983ad328962c38c3744cb9
RGB Studios <justingolden21@gmail.com>
2018-12-16 23:10:03 -0800

    add sticky header, fix unlock requirements

--- scripts/unlocks.js
+++ scripts/unlocks.js
@@ -9,26 +9,26 @@ let big_fish_unlocked = false;
 let aquarium_unlocked = false;
 
 function checkUnlocks() {
-	if(!sell_small_fish_unlocked && small_fish.length > 75) {
+	if(!sell_small_fish_unlocked && small_fish.length >= 75) {
 		sell_small_fish_unlocked = true;
 		unlock($('#sell-small-fish') );
 		// showHighlight($('#sell-small-fish') );
 		showSnackbar('Unlocked ability to sell small fish', 'success', UNLOCK_SNACKBAR_TIME);
 	}
-	if(!sell_medium_fish_unlocked && medium_fish.length > 75) {
+	if(!sell_medium_fish_unlocked && medium_fish.length >= 75) {
 		sell_medium_fish_unlocked = true;
 		unlock($('#sell-medium-fish') );
 		// showHighlight($('#sell-medium-fish') );
 		showSnackbar('Unlocked ability to sell medium fish', 'success');
 	}
-	if(!sell_big_fish_unlocked && big_fish.length > 75) {
+	if(!sell_big_fish_unlocked && big_fish.length >= 75) {
 		sell_big_fish_unlocked = true;
 		unlock($('#sell-big-fish') );
 		// showHighlight($('#sell-big-fish') );
 		showSnackbar('Unlocked ability to sell big fish', 'success');
 	}
 
-	if(!medium_fish_unlocked && small_fish.length > 50) {
+	if(!medium_fish_unlocked && small_fish.length >= 50) {
 		medium_fish_unlocked = true;
 		unlock($('#my-medium-fish') );
 		unlock($('#purchase-medium-fish') );
@@ -36,7 +36,7 @@ function checkUnlocks() {
 		// showHighlight($('#purchase-medium-fish') );
 		showSnackbar('Unlocked medium fish', 'success');	
 	}
-	if(!big_fish_unlocked && medium_fish.length > 50) {
+	if(!big_fish_unlocked && medium_fish.length >= 50) {
 		big_fish_unlocked = true;
 		unlock($('#my-big-fish') );
 		unlock($('#purchase-big-fish') );
@@ -44,7 +44,7 @@ function checkUnlocks() {
 		// showHighlight($('#purchase-big-fish') );
 		showSnackbar('Unlocked big fish', 'success');
 	}
-	if(!aquarium_unlocked && num_aquarium_space_used > AQUARIUM_SPACE/2) {
+	if(!aquarium_unlocked && num_aquarium_space_used >= AQUARIUM_SPACE/2) {
 		aquarium_unlocked = true;
 		unlock($('#my-aquarium') );
 		unlock($('#purchase-aquarium') );
