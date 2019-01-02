dd8879a68f1cccb14fe2a214eaf546f24321f6d8
Justin Golden <justingolden21@gmail.com>
2019-01-02 01:44:57 -0800

    testing number.js

--- scripts/number.js
+++ scripts/number.js
@@ -2,9 +2,33 @@
 
 let places = 3;
 
+// https://officespace.zendesk.com/hc/en-us/articles/115000593531-Scientific-Notation-Large-Numbers-Guide
+let num_abrev = 'M B T Qa Qi Sx Sp Oc No Dc Ud Td'.split(' ');
+
 function prettyPrintNum(num) {
 	if(num<1e6)
 		return num.toLocaleString(); //adds commas
+
+	console.log(num/Math.pow(10, Math.floor( (numDigits(num)-1)/3)+4) );
+	console.log(num_abrev[Math.floor( (numDigits(num)-1)/3)-2] );
+
+/*
+
+floor( (num-1)/3)+4
+
+num digits: divide by 10**this
+7: 6
+8: 6
+9: 6
+
+10: 7
+11: 7
+12: 7
+
+13: 8
+14: 8
+15: 8
+*/
 
 	if(num<1e9)
 		return round(num/1e6, places).toString() + "M";
@@ -33,10 +57,15 @@ function prettyPrintNum(num) {
 
 	return round(num/1e39, places).toString() + "Td";
 
-// https://officespace.zendesk.com/hc/en-us/articles/115000593531-Scientific-Notation-Large-Numbers-Guide
+
 
 }
 
 function round(num, places) {
 	return Math.round(num * Math.pow(10,places) ) / Math.pow(10,places);
+}
+
+// https://stackoverflow.com/questions/14879691/get-number-of-digits-with-javascript
+function numDigits(num) {
+	return Math.max(Math.floor(Math.log10(Math.abs(num) ) ), 0) + 1;
 }
