0f6d4895117601811e6f95f5c0bcb7ccb743c388
Justin Golden <justingolden21@gmail.com>
2019-11-13 15:10:11 -0800

    wheel works

--- public/scripts/personal-wheel.js
+++ public/scripts/personal-wheel.js
@@ -6,6 +6,8 @@ let wheel_canvas, wheel_ctx;
 
 let wheel_slices = [10,5,10,25,10,5,10,50];
 let wheel_colors = 'blue red blue green blue red blue green'.split(' ');
+// wheel_slices = [0,1,2,3,4,5,6,7]; // for testing
+
 let wheel_rotation = 0;
 
 const WHEEL_COLOR = 'yellow';
@@ -15,19 +17,22 @@ const WHEEL_FONT_COLOR = 'white';
 
 const WHEEL_RADIUS = 250;
 const INNER_WHEEL_RADIUS = 220;
-const WHEEL_ROTATE_RATE = 10;
+let wheel_rotate_rate = degreesToRadians(random(20,40) );
+const WHEEL_SLOWDOWN_RATE = degreesToRadians(1);
+let total_wheel_rotation = 0;
+let done_spinning = false;
 
 $( ()=> {
 	// tmp, toggle comment for testing and prod
-	// $('#personal-wheel-modal').modal('show');
-	// setInterval(updateWheel, 100);
+	$('#personal-wheel-modal').modal('show');
+	setInterval(updateWheel, 100);
 
 	wheel_canvas = document.getElementById('personal-wheel');
 	wheel_ctx = wheel_canvas.getContext('2d');
 	wheel_canvas.width = 500;
 	wheel_canvas.height = 500;
 
-	wheel_ctx.textAlign = 'right';
+	wheel_ctx.textAlign = 'center';
 	wheel_ctx.font = WHEEL_FONT;
 
 });
@@ -49,21 +54,7 @@ function updateWheel() {
 		wheel_ctx.arc(wheel_canvas.width/2, wheel_canvas.height/2, INNER_WHEEL_RADIUS, start_angle, end_angle);
 		wheel_ctx.lineTo(wheel_canvas.width/2, wheel_canvas.height/2);
 		wheel_ctx.fillStyle = wheel_colors[i];
-		wheel_ctx.fill();
-
-
-
-
-
-
-
-		//todo move with wheel_rotation
-
-		// rotate
-		// wheel_rotation += WHEEL_ROTATE_RATE;
-		// wheel_rotation %= 360;
-
-		// wheel_ctx.rotate(WHEEL_ROTATE_RATE);
+		wheel_ctx.fill();		
 	}
 
 	for(let i=0; i<wheel_slices.length; i++) {
@@ -71,19 +62,41 @@ function updateWheel() {
 		let start_angle = (i * 2*Math.PI/wheel_slices.length); // same as above, new loop so text on top
 		let half_rotation = 2*Math.PI/wheel_slices.length/2;
 		wheel_ctx.fillStyle = WHEEL_FONT_COLOR;
-		let pos = rotate(wheel_canvas.width/2, wheel_canvas.height/2, wheel_canvas.width/2, WHEEL_FONT_SIZE+20, start_angle-half_rotation);
+		let pos = rotate(wheel_canvas.width/2, wheel_canvas.height/2, wheel_canvas.width/2, WHEEL_FONT_SIZE+40, start_angle-half_rotation);
 		wheel_ctx.fillText(wheel_slices[i], pos.x, pos.y);	
 	}
 
+	wheel_ctx.translate(wheel_canvas.width/2, wheel_canvas.width/2);
+	wheel_ctx.rotate(wheel_rotate_rate);
+	wheel_ctx.translate(-wheel_canvas.width/2, -wheel_canvas.width/2);
+
+	total_wheel_rotation += wheel_rotate_rate;
+	wheel_rotate_rate -= WHEEL_SLOWDOWN_RATE;
+	if(wheel_rotate_rate <= 0) {
+		wheel_rotate_rate = 0;
+		if(!done_spinning) {
+			done_spinning = true;
+
+			let slice_num = getSliceNum(total_wheel_rotation, wheel_slices.length);
+			winWheel(wheel_slices[slice_num]);
+		}
+	}
 }
 
-function getTextPosition(oldX, oldY, sliceNum, numSlices) {
-	let x = oldX * Math.cos(sliceNum/numSlices*2*Math.PI);
-	let y = oldY * Math.sin(sliceNum/numSlices*2*Math.PI);
-	return {x: x, y:y};
+// get shell, open modal alert that you got shells
+function winWheel(num_shell) {
+	showAlert('Congrats!', 'Congratulations! You won ' + num_shell
+		+ ' gold shells' + getImgStr('shell-gold.png','icon-sm') );
+	num_gold_shell += num_shell;
 }
 
+function getSliceNum(rad, num_slices) {
+	rad %= 2*Math.PI;
 
+	let section_size = 2*Math.PI / num_slices;
+
+	return Math.ceil(rad/section_size);
+}
 
 // https://stackoverflow.com/questions/17410809/how-to-calculate-rotation-in-2d-in-javascript
 // angle in radians
@@ -97,13 +110,6 @@ function rotate(cx, cy, x, y, angle) {
 
 
 function degreesToRadians(deg) {
-    return deg * Math.PI / 180;
+	return deg * Math.PI / 180;
 }
 
-function sumTo(a, i) {
-    var sum = 0;
-    for (var j = 0; j < i; j++) {
-      sum += a[j];
-    }
-    return sum;
-}
